e87f2cb25f622359c6869651d4735b67666b7d87
Crowdbotics <team@crowdbotics.com>
2021-12-20 03:25:12 +0000

    Initial commit.

---
+++ backend/fbs_plus_32496/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fbs_plus_32496 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fbs_plus_32496.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "fbs_plus_32496",
+  url: "https://fbs_plus_32496.botics.co",
+  api: "https://fbs_plus_32496.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
